2412bd15308508abb2df902b319605d941123d92
Chris Hill <v-hichr@microsoft.com>
2018-10-03 23:58:26 -0700

    10/3 Night

---
+++ SandboxMain/Sandbox/QuickSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class QuickSort
+    {
+        
+        
+        static public void SortQuick(int[] arr, int left, int right)
+        {
+            if (left >= right) return;
+            int pivot = arr[(left + right) / 2]; // get pivot value
+            int index = partition(arr, left, right, pivot); // get partition point
+            SortQuick(arr, left, index - 1); // recurse on left side
+            SortQuick(arr, index, right); // recurse on right side
+        }
+
+        public static int partition(int[] array, int left, int right, int pivot)
+        {
+            while (left <= right) // move pointers in towards each other
+            {
+                while (array[left] < pivot) left++; // move left pointer
+                while (array[right] > pivot) right--; // move right pointer
+                if (left <= right) // if the left pointer is less than the right pointer
+                {   
+                    int t = array[left];
+                    array[left] = array[right];
+                    array[right] = t;
+                    left++;
+                    right--;
+                }
+            }
+            return left; //return partition point.
+        }
+    }
+}
--- SandboxMain/Sandbox/Sandbox.cs
+++ SandboxMain/Sandbox/Sandbox.cs
@@ -11,49 +11,9 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
-            int[] myArray = new int[] { 2, 8, 3, 5, 4, 9, 6 };
-            myArray = QuickSort(myArray, 0, myArray.Length - 1);
+            int[] myArray = new int[] { 4, 2, 1, 3 };
+            QuickSort.QuickSort.SortQuick(myArray, 0, myArray.Length - 1);
             Console.ReadLine();
-        }
-        
-        static int[] QuickSort(int[] inArray, int left, int right)
-        {
-            int pivot = (left + right) / 2;
-            int l = left, r = right;
-            while (l < r)
-            {
-                while (inArray[l] < inArray[pivot])
-                {
-                    l++;
-                }
-                while (inArray[r] > inArray[pivot])
-                {
-                    r--;
-                }
-                if (l < r)
-                {
-                    inArray[l] = inArray[l] + inArray[r];
-                    inArray[r] = inArray[l] - inArray[r];
-                    inArray[l] = inArray[l] - inArray[r];
-                    l++;
-                    r--;
-                }
-            }
-            if (left < pivot)
-            {
-                QuickSort(inArray, left, pivot);
-                QuickSort(inArray, pivot, right);
-            }
-            return inArray;
-        }
-
-        static int[] SwapArray(int[] inArray, int a, int b)
-        {   
-            inArray[a] = inArray[a] + inArray[b];
-            inArray[b] = inArray[a] - inArray[b];
-            inArray[a] = inArray[a] - inArray[b];
-
-            return inArray;
         }
     }
 }
