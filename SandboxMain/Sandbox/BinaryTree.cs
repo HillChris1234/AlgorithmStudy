15b3f38fc0928b19fc13dfeebdd200c260457854
REDMOND\v-hichr <v-hichr@microsoft.com>
2018-10-09 17:57:39 -0700

    BinaryTree

---
+++ SandboxMain/Sandbox/BinaryTree.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Node
+    {
+        Node left, right;
+        int data;
+        public Node(int val)
+        {
+            this.data = val;
+        }
+        public void Insert(int val)
+        {
+            if (val <= data)
+            {
+                if (left == null)
+                {
+                    left = new Node(val);
+                }
+                else
+                {
+                    left.Insert(val);
+                }
+            }
+            else
+            {
+                if (right == null)
+                {
+                    right = new Node(val);
+                }
+                else
+                {
+                    right.Insert(val);
+                }
+            }
+        }
+        public bool Contains(int v)
+        {
+            if (v == data) return true;
+            if (v < data)
+            {
+                return left == null ? false : left.Contains(v);
+            }
+            else
+            {
+                return right == null ? false : right.Contains(v);
+            }
+        }
+
+        public void PrintInOrder()
+        {
+            if (left != null) left.PrintInOrder(); 
+            Console.WriteLine(data);
+            if (right != null) right.PrintInOrder();
+        }
+    }
+}
--- SandboxMain/Sandbox/Sandbox.cs
+++ SandboxMain/Sandbox/Sandbox.cs
@@ -11,9 +11,93 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
-            int[] myArray = new int[] { 4, 2, 1, 3 };
-            QuickSort.QuickSort.SortQuick(myArray, 0, myArray.Length - 1);
-            Console.ReadLine();
+            int[] myArray = new int[] { 3, 2, 4, 1, 5 };
+            Node myTree = new Node(myArray[0]);
+            for (int x = 1; x < myArray.Length; x++)
+            {
+                myTree.Insert(myArray[x]);
+            }
+
+            bool doesContain = myTree.contains(1);
+
+            myTree.printInOrder();
+            Console.ReadLine();    
+        }
+        
+    }
+    
+    class Node
+    {
+        Node left, right;
+        int data;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+        public void Insert(int value)
+        {
+            if (value <= data)
+            {
+                if (left == null)
+                {
+                    left = new Node(value);
+                } else
+                {
+                    left.Insert(value);
+                }
+            } else
+            {
+                if (right == null)
+                {
+                    right = new Node(value);
+                }
+                else
+                {
+                    right.Insert(value);
+                }
+            }
+        }
+        public Boolean contains(int value)
+        {
+            if (value == data)
+            {
+                return true;
+            }
+            else if (value < data)
+            {
+                if (left == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    return left.contains(value);
+                }
+            }
+            else
+            {
+                if (right == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    return right.contains(value);
+                }
+            }
+        }
+
+        public void printInOrder()
+        {
+            if (left != null)
+            {
+                left.printInOrder();
+            }
+            Console.WriteLine(data);
+            if (right != null)
+            {
+                right.printInOrder();
+            }
         }
     }
 }
