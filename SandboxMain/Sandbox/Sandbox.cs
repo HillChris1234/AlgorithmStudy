6cbc3f128f8990e36643995a277a68d7ff91f15c
Chris Hill <v-hichr@microsoft.com>
2018-10-18 08:31:25 -0700

    DFS

--- SandboxMain/Sandbox/Sandbox.cs
+++ SandboxMain/Sandbox/Sandbox.cs
@@ -11,46 +11,86 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
-            Map m = new Map();
+            Graph.Node n1 = new Graph.Node(1);
+            Graph.Node n2 = new Graph.Node(2);
+            Graph.Node n3 = new Graph.Node(3);
+            Graph.Node n4 = new Graph.Node(4);
+            Graph.Node n5 = new Graph.Node(5);
+            Graph.Node n6 = new Graph.Node(6);
+            n1.adjacent.AddLast(n2);
+            n1.adjacent.AddLast(n3);
+            n2.adjacent.AddLast(n4);
+            n2.adjacent.AddLast(n5);
+            n2.adjacent.AddLast(n6);
+            Graph.
             Console.ReadLine();
         }
 
-        class Node
+        public class Employee
         {
-            public Node down, right;
-            public string state;
-            public Node(string s)
+            public string name;
+            public List<Employee> Employees;
+            public Employee(string name)
             {
-                state = s;
+                this.name = name;
+            }
+            public void isEmployeeOf(Employee e)
+            {
+                this.Employees.Add(e);
             }
         }
 
-        public class Map
+
+
+
+        public class Graph
         {
-            Node head;
-            public Map()
+            private Dictionary<int, Node> nodeLookUp = new Dictionary<int, Node>();
+            public class Node
             {
-                head = new Node("");
-                Node cur = head;
-                for (int y = 1; y < 3; y++)
+                public int id;
+                public LinkedList<Node> adjacent = new LinkedList<Node>();
+                public Node (int id)
                 {
-                    cur.down = new Node("");
-                    cur = cur.down;
+                    this.id = id;
                 }
-                head.right = new Node("");
-                cur = head.right;
-                for (int y = 1; y < 3; y++)
+            }
+            public Node getNode(int id)
+            {
+                return nodeLookUp[id];
+            }
+
+            public void addEdge(int source, int destination)
+            {
+                Node s = getNode(source);
+                Node d = getNode(destination);
+                s.adjacent.AddLast(d);
+            }
+
+            public bool hasPathDFS(int source, int destination)
+            {
+                Node s = getNode(source);
+                Node d = getNode(destination);
+                List<int> visited = new List<int>();
+                return hasPathDFS(s, d, visited);
+            }
+
+            private bool hasPathDFS(Node source, Node destination, List<int> visited)
+            {
+                if (visited.Contains(source.id)) return false;
+                visited.Add(source.id);
+                if (source == destination)
                 {
-                    cur.down = new Node(y == 1 ? "block" : "");
-                    cur = cur.down;
+                    return true;
                 }
-                head.right.right = new Node("");
-                cur = head.right.right;
-                for (int y = 1; y < 3; y++)
+                foreach (Node child in source.adjacent)
                 {
-                    cur.down = new Node(y == 2 ? "end" : "");
-                    cur = cur.down;
+                    if (hasPathDFS(child, destination, visited))
+                    {
+                        return true;
+                    }
                 }
+                return false;
             }
         }
     }
