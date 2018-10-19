a785a6c1e6370291e74befc60a8d0a5fe35eecf4
REDMOND\v-hichr <v-hichr@microsoft.com>
2018-10-18 18:27:20 -0700

    DFS

--- SandboxMain/Sandbox/Sandbox.cs
+++ SandboxMain/Sandbox/Sandbox.cs
@@ -7,92 +7,78 @@ using System.Threading.Tasks;
 
 namespace Sandbox
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static Node sample_tree()
         {
-            Graph.Node n1 = new Graph.Node(1);
-            Graph.Node n2 = new Graph.Node(2);
-            Graph.Node n3 = new Graph.Node(3);
-            Graph.Node n4 = new Graph.Node(4);
-            Graph.Node n5 = new Graph.Node(5);
-            Graph.Node n6 = new Graph.Node(6);
-            n1.adjacent.AddLast(n2);
-            n1.adjacent.AddLast(n3);
-            n2.adjacent.AddLast(n4);
-            n2.adjacent.AddLast(n5);
-            n2.adjacent.AddLast(n6);
-            Graph.
-            Console.ReadLine();
+            Node root = 
+                new Node("A",
+                    new Node("B",
+                        new Node("C"), new Node("D")),
+                    new Node("E",
+                        new Node("F"), new Node("G",
+                                                new Node("H"), null)));
+            return root;
+        }
+        public static void Main(string[] args)
+        {
+            Node tree = sample_tree();
+            Console.WriteLine("BFS --> "); bfs_traversal(tree);
         }
 
-        public class Employee
+        static void bfs_traversal(Node node)
         {
-            public string name;
-            public List<Employee> Employees;
-            public Employee(string name)
+            Queue<Node> q = new Queue<Node>();
+            q.Enqueue(node);
+            while (q.Count > 0)
             {
-                this.name = name;
-            }
-            public void isEmployeeOf(Employee e)
-            {
-                this.Employees.Add(e);
-            }
-        }
-
-
-
-
-        public class Graph
-        {
-            private Dictionary<int, Node> nodeLookUp = new Dictionary<int, Node>();
-            public class Node
-            {
-                public int id;
-                public LinkedList<Node> adjacent = new LinkedList<Node>();
-                public Node (int id)
-                {
-                    this.id = id;
-                }
-            }
-            public Node getNode(int id)
-            {
-                return nodeLookUp[id];
-            }
-
-            public void addEdge(int source, int destination)
-            {
-                Node s = getNode(source);
-                Node d = getNode(destination);
-                s.adjacent.AddLast(d);
-            }
-
-            public bool hasPathDFS(int source, int destination)
-            {
-                Node s = getNode(source);
-                Node d = getNode(destination);
-                List<int> visited = new List<int>();
-                return hasPathDFS(s, d, visited);
-            }
-
-            private bool hasPathDFS(Node source, Node destination, List<int> visited)
-            {
-                if (visited.Contains(source.id)) return false;
-                visited.Add(source.id);
-                if (source == destination)
-                {
-                    return true;
-                }
-                foreach (Node child in source.adjacent)
-                {
-                    if (hasPathDFS(child, destination, visited))
-                    {
-                        return true;
-                    }
-                }
-                return false;
+                node = q.Dequeue();
+                Console.Write(node.data + " ");
+                
+                if (node.left != null)
+                    q.Enqueue(node.left);
+                if (node.right != null)
+                    q.Enqueue(node.right);
             }
         }
     }
-    
+
+    public class Node
+    {
+        public string data;
+        public List<Node> nodes;
+        public Node(string data, List<Node> nodes)
+        {
+            this.data = data;
+            foreach(Node n in nodes)
+            {
+                this.nodes.Add(n);
+            }
+        }
+        public Node(string data)
+        {
+            this.data = data;
+            this.nodes = null;
+        }
+    }
+
+    public class NodeBinary
+    {
+        public Node left;
+        public Node right;
+        public string data;
+        public NodeBinary(string data, Node left, Node right)
+        {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+
+        public NodeBinary(string data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
 }
