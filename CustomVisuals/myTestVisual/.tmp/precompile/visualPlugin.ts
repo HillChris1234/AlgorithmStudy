996110e3d9d92262dcbbbc144e36e07f19080d26
Christine Hill <Christine.Hill105@T-Mobile.com>
2018-12-27 10:05:57 -0800

    Adding Custom Visual Work

---
+++ CustomVisuals/myTestVisual/.tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var myTestVisual4A9E4A074F544F6DBB875E12C9FCEF9B_DEBUG = {
+        name: 'myTestVisual4A9E4A074F544F6DBB875E12C9FCEF9B_DEBUG',
+        displayName: 'myTestVisual',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '2.2.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.myTestVisual4A9E4A074F544F6DBB875E12C9FCEF9B.Visual(options),
+        custom: true
+    };
+}
