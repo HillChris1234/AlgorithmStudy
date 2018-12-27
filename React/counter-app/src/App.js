1f93fa3efa0d0cd55003106f9955753987c2b15c
HillChris1234 <hillchris1234@gmail.com>
2018-12-26 16:52:35 -0800

    Adding React Tutorial

---
+++ React/counter-app/src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import NavBar from "./components/navBar";
+import Counters from "./components/counters";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  constructor() {
+    super();
+    console.log("App - Constructor");
+  }
+
+  componentDidMount() {
+    console.log("App - Mounted");
+  }
+
+  handleReset = () => {
+    const counters = this.state.counters.map(x => {
+      x.value = 0;
+      return x;
+    });
+    this.setState(counters);
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDelete = id => {
+    const counters = this.state.counters.filter(x => x.id !== id);
+    this.setState({ counters });
+  };
+
+  render() {
+    console.log("App - Rendered");
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
